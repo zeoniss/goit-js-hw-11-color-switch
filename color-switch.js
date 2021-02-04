eedd7e11fd6fee28d4b036cb2bc521c8f5083591
Vladimir Kriukov <zeoniss28@gmail.com>
2021-02-04 23:23:29 +0200

    Initial commit

---
+++ color-switch.js
@@ -0,0 +1,12 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
